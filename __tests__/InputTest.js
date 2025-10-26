9bf3c816da89737426ca55d49083bf7696efc82e
JeongBeanHyun <kelly92108@gmail.com>
2025-10-26 22:07:15 +0900

    test(input) : 자동차 이름 및 경주 시도 횟수 입력 테스트 추가

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,12 +1,34 @@
-describe("inputCarNames 테스트", () => {
-  test("자동차 이름 문자열을 배열로 변환", () => {
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { inputCarNames } from "../src/input/inputCarNames";
+import { inputRaceCount } from "../src/input/inputRaceCount";
+
+const mockQuestions = (input) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    return Promise.resolve(input);
+  });
+};
+
+describe("입력 테스트", () => {
+  test("자동차 이름 정상 입력", async () => {
     //given
-    const cars = "jb, hb, lhj";
+    const cars = "jb,hb,lhj";
+    mockQuestions(cars);
 
     //when
-    const carNames = parseCarNames(cars);
+    const input = await inputCarNames();
 
     //then
-    expect(carNames).toEqual(["jb", "hb", "lhj"]);
+    expect(input).toBe("jb,hb,lhj");
+  });
+
+  test("자동차 경주 시도할 횟수 정상 입력", async () => {
+    const count = "5";
+    mockQuestions(count);
+
+    const input = await inputRaceCount();
+
+    expect(input).toBe("5");
   });
 });
