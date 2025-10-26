5bd8ea7d450a2b1ff63f2765411254496d99339a
JeongBeanHyun <kelly92108@gmail.com>
2025-10-26 22:09:07 +0900

    feat(input) : 자동차 이름 및 경주 시도 횟수 입력 기능 추가

---
+++ src/input/inputRaceCount.js
@@ -0,0 +1,6 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export const inputRaceCount = async () => {
+  const count = Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  return count;
+};
